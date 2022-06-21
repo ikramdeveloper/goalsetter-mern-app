317b5a3cbde85d0752827eed378b09ae4cb3c95d
MuhammadIkram24 <m.ikramch22@gmail.com>
2022-06-21 20:55:21 +0500

    added readme file

---
+++ README.md
@@ -0,0 +1,27 @@
+# Goal Setter MERN App
+
+### Frontend
+
+> ReactJS
+
+> Axios
+
+> React Router DOM v6
+
+> Redux Toolkit
+
+> React Icons
+
+> React Toastify
+
+### Backend
+
+> Bcryptjs
+
+> Express
+
+> Express Async Handler
+
+> JSON Web Token
+
+> Mongoose
--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === "production") {
 
   app.get("*", (req, resp) => {
     resp.sendFile(
-      path.resolve(__dirname, "..", "frontend", "build", "index.html")
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
     );
   });
 } else {
